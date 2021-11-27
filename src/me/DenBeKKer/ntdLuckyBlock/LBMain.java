6098da76f7bc3d3af2a56b27a86cce10624f2549
Den Bekker <69750264+danirod12@users.noreply.github.com>
2021-11-27 21:37:05 +0300

    2.4.4 ice fix

--- src/me/DenBeKKer/ntdLuckyBlock/LBMain.java
+++ src/me/DenBeKKer/ntdLuckyBlock/LBMain.java
@@ -85,8 +85,8 @@ public class LBMain extends JavaPlugin {
 	
 	
 	// Last update date & Build number
-	private static final String last_update = "22/11/2021";
-	private static final int build = 68;
+	private static final String last_update = "27/11/2021";
+	private static final int build = 69;
 	// Last update date & Build number
 	
 	
