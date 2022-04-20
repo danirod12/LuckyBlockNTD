56d047560009556ea8af38f8d404de0fa3d09cd7
Den Bekker <69750264+danirod12@users.noreply.github.com>
2022-04-20 19:41:09 +0300

    2.6.3 RELEASE

--- plugin/src/main/java/me/DenBeKKer/ntdLuckyBlock/LBMain.java
+++ plugin/src/main/java/me/DenBeKKer/ntdLuckyBlock/LBMain.java
@@ -81,8 +81,8 @@ public class LBMain extends JavaPlugin {
 	
 	
 	// Last update date & Build number
-	private static final String last_update = "05/04/2022";
-	private static final int build = 92;
+	private static final String last_update = "20/04/2022";
+	private static final int build = 93;
 	// Last update date & Build number
 	
 	
