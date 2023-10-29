52e78df08f18978b09155cc4121084e305f0a1bc
Den Bekker <danfedorov0@gmail.com>
2023-10-29 14:59:12 +0300

    Fixed inconsistent skull meta warning on newer MC versions

--- plugin/src/main/java/me/DenBeKKer/ntdLuckyBlock/util/Misc.java
+++ plugin/src/main/java/me/DenBeKKer/ntdLuckyBlock/util/Misc.java
@@ -13,6 +13,8 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
@@ -154,9 +156,17 @@ public class Misc {
         profile.getProperties().put("textures", new Property("textures",
                 new String(Base64.getEncoder().encode(("{textures:{SKIN:{url:\"" + url + "\"}}}").getBytes()))));
         try {
-            Field profileField = headMeta.getClass().getDeclaredField("profile");
-            profileField.setAccessible(true);
-            profileField.set(headMeta, profile);
+            try {
+                // New method that modifies profile and serializedProfile
+                Method method = headMeta.getClass().getDeclaredMethod("setProfile", GameProfile.class);
+                method.setAccessible(true);
+                method.invoke(headMeta, profile);
+            } catch (NoSuchMethodException | InvocationTargetException exception) {
+                // Old method that modifies profile
+                Field profileField = headMeta.getClass().getDeclaredField("profile");
+                profileField.setAccessible(true);
+                profileField.set(headMeta, profile);
+            }
         } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException e) {
             e.printStackTrace();
         }
