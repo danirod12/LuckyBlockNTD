e2b85862d75d7cf4637983e5eb695f37e8c05a6b
Den Bekker <danfedorov0@gmail.com>
2024-04-27 00:38:28 +0300

    Fixed water bucket suffocation

--- plugin/src/main/java/me/DenBeKKer/ntdLuckyBlock/variables/drop/special/WaterBucketSpecial.java
+++ plugin/src/main/java/me/DenBeKKer/ntdLuckyBlock/variables/drop/special/WaterBucketSpecial.java
@@ -4,8 +4,10 @@ import com.google.gson.annotations.SerializedName;
 import me.DenBeKKer.ntdLuckyBlock.LBMain;
 import me.DenBeKKer.ntdLuckyBlock.util.manager.MessagesManager.Message;
 import me.DenBeKKer.ntdLuckyBlock.variables.LuckyDrop;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -29,8 +31,17 @@ public class WaterBucketSpecial implements LuckyDrop {
         } else {
             target.sendMessage(Message.WATER_BUCKET.getAsString());
             target.getInventory().addItem(new ItemStack(Material.WATER_BUCKET));
-            target.teleport(target.getLocation().add(0, h, 0));
+            Location locationTarget = target.getLocation().clone().add(0.0, h, 0.0);
+            Block block = locationTarget.getBlock();
+            if (!block.getType().isAir() || !block.getRelative(BlockFace.UP).getType().isAir()) {
+                double x = locationTarget.getX();
+                double z = locationTarget.getZ();
+                locationTarget = block.getWorld().getHighestBlockAt(locationTarget).getLocation();
+                locationTarget.setX(x);
+                locationTarget.setZ(z);
+                locationTarget.setY(locationTarget.getY() + 1 + h);
+            }
+            target.teleport(locationTarget);
         }
     }
-
 }
